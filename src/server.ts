af44635110d80c0b8cf1f1c79e88443cd9f6b88d
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-05-14 23:52:03 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", () => {
+	return "Hello, world!";
+});
+
+app
+	.listen({
+		port: 8080
+	})
+	.then(() => console.log("HTTP Server Running!"));
